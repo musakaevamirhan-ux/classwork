8b926363cf7eaccd51114e3f28623043c87b7b18
Взрослая академия <georgygevorkov@gmail.com>
2026-06-02 21:17:13 +0300

    условия

---
+++ 02.06.py
@@ -0,0 +1,24 @@
+# a = int(input())
+# t = a%10
+# r = a//1000
+# k = (a%1000)//100
+# g = (a%100)//10
+# print(t*1000 + g*100 + k*10 + r)
+# a = int(input())
+# if a%2 == 0:
+#     print("четное")
+# else:
+#     print("нечетное")
+# a = int(input())
+# b = int(input())
+# if a>b:
+#     print(a)
+# elif b>a:
+#     print(b)
+# elif a == b:
+#     print("равны")
+# a = input()
+# if a == "Ваня" or a == "ваня":
+#     print("Ввел")
+# else:
+#     print("Не ввел")
