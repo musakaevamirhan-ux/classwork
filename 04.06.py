e4c37f666912fff65e269917060982534178ef48
Взрослая академия <georgygevorkov@gmail.com>
2026-06-04 21:08:39 +0300

    Логические операции

---
+++ 04.06.py
@@ -0,0 +1,52 @@
+# 1
+# a = int(input())
+# b = int(input())
+# if a > b:
+#     print(a)
+# elif b > a:
+#     print(b)
+# elif a == b:
+#     print("числа равны")
+# 2
+# s = int(input("расстояние:"))
+# t = int(input("время:"))
+# print(s/t)
+# 3
+# k = int(input())
+# x = k // 100
+# y = k%10
+# g = k%100 // 10
+# print(y*100 + g*10 + x)
+# 4
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a>b:
+#     if a>c:
+#         print(a)
+# if b>a:
+#     if b>c:
+#         print(b)
+# if c>b:
+#     if c>a:
+#         print(c)
+# elif a == b == c:
+#     print(a)
+# 5.1
+# k = int(input())
+# if 0<=k<=10:
+#     print("YES")
+# else:
+#     print("NO")
+# 5.2
+# if k >= 0 and k <= 10:
+#     print("YES")
+# else:
+#     print("NO")
+# 5.3
+# if not(k>= 10 or k<= 0):
+#     print("YES")
+# else:
+#     print("NO")
+
+
