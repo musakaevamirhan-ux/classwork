7b09e26bc27c504ec3a7df515738cbd8a92e25df
Взрослая академия <georgygevorkov@gmail.com>
2026-06-11 21:02:47 +0300

    Циклы

---
+++ 11.06.py
@@ -0,0 +1,65 @@
+from math import factorial
+# 1
+# a = int(input())
+# b = int(input())
+# if a > b:
+#     a, b = b, a
+# sum = 0
+# while a <= b:
+#     sum += a
+#     a += 1
+# print(sum)
+# 2
+# a = int(input())
+# b = int(input())
+# if b > a:
+#     a, b = b, a
+# while  a>=b:
+#     print(a, end=" ")
+#     a -= 1
+# 3
+# max = 0
+# while True:
+#     a = int(input())
+#     if a == 0:
+#         break
+#     if a > max:
+#         max = a
+# if max == 0:
+#     max = a
+# print(max)
+# 4
+# a = int(input())
+# b = int(input())
+# a1 = int(input())
+# b1 = int(input())
+# if a > b:
+#     a, b = b, a
+# if a1 > b1:
+#     a1, b1 = b1, a1
+# if a1 > a:
+#     a, a1 = a1, a
+# if b > b1:
+#     b, b1 = b1, b
+# while a <= b:
+#     if a1<=a<=b1:
+#         print(a,end=" ")
+#     a += 1
+# a = int(input())
+# print(a%2 == 0 and "YES" or "NO")
+# a = int(input())
+# b = int(input())
+# proizved = 1
+# for i in range(a,b+1):
+#     proizved *= i
+# print(proizved)
+# if a > b:
+#     a, b = b, a
+# print(a*factorial(b)/factorial(a))
+#
+# n = int(input())
+# for i in range(n):
+#     a = int(input())
+#     if a%2 == 0:
+#         print(a,end=" ")
+
